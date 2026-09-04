331fd798a6340c8ea17eb4e8bbb7ce829d9b74f1
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-04 11:19:05 +0200

    feat: add Mutation component for fetching and displaying posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 
 import Examples from "./comps/Examples.jsx";
+import Mutation from "./comps/Mutation.jsx";
 
 function App() {
   return (
     <>
       <h1>Welcome to Vite!</h1>
       <Examples />
+      <Mutation />
     </>
   );
 }
--- src/comps/Mutation.jsx
+++ src/comps/Mutation.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
 async function fetchPosts() {
@@ -5,24 +6,22 @@ async function fetchPosts() {
   return response.json();
 }
 
-const Examples = () => {
-  const { data, isLoading, error, refetch } = useQuery({
+const Mutation = () => {
+  const { data, isLoading, error } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
 
   return (
     <div>
-      <h2>Examples</h2>
-
+      <h2>Mutation</h2>
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
       {data?.map((post) => (
         <li key={post.id}>{post?.title}</li>
       ))}
-      <button onClick={() => refetch()}>Refetch</button>
     </div>
   );
 };
 
-export default Examples;
+export default Mutation;
