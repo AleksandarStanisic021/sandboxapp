0bf1cd015a1ceb1bbcd066a63b13ccd947f4b4d5
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-04 10:46:29 +0200

    refactor: replace useEffect and useState with useQuery for fetching posts

--- src/comps/Examples.jsx
+++ src/comps/Examples.jsx
@@ -1,30 +1,23 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 async function fetchPosts() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  const res = await response.json();
-  return res;
+  return response.json();
 }
 
 const Examples = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchPosts();
-      setPosts(data);
-    };
-    fetchData();
-  }, []);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
 
   return (
     <div>
       <h2>Examples</h2>
 
-      {posts.map((post) => (
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data?.map((post) => (
         <li key={post.id}>{post?.title}</li>
       ))}
     </div>
